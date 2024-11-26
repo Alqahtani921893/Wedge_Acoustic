9102e49c4a051c8856ba1fe47201e03f191da83a
Alqahtani921893 <hhnnll@yahoo.com>
2024-11-26 20:00:05 +0300

    Remove code structure section and clean up main directory

--- u_d_l_m.m
+++
@@ -1,9 +0,0 @@
-%function # 2
-function u_d_l_m_value = u_d_l_m(l,m)
-    global r_ phi_ r phi  k 
-    A=(exp(-1j*k*r_)/sqrt(k*r_));
-    B=(exp(-1j*k*r)/sqrt(k*r));
-    C= P_l_m(l,m,phi,phi_);
-    D=F(a_l(l)*(k*r*r_)/(r+r_));
-    u_d_l_m_value = A* B*C*D;
-end
